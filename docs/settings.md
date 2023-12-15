fd2d7bc4bb174d8602ce7dfd55d5ecddd646fcbd
Alfred Narjes <alfred.narjes@gmail.com>
2023-12-15 02:03:58 +0100

    setting icon added, added padding to the content so small devices can access settings, TODO: seperate carousel and description for small devices

--- docs/settings.md
+++ docs/settings.md
@@ -2,10 +2,11 @@
 <link rel="stylesheet" href="./projects.css">
 
 <div id="copy-popup" class="hov-desc2 d-none" ></div>
-## Auto copy
 
-<div class="d-flex">
-<i class="mdi mdi-toggle-switch"></i>
+
+<div class="d-flex a-center m-lr-0 mbt-0">
+<!-- <i class="mdi mdi-toggle-switch"></i> -->
+<h2 id="auto-copy" class="m m-t-0-imp m-lr-0-imp m-b-2-imp">Auto copy</h2>
 <svg id="copy-toggle-on" style="width: 180px; height: 100px;" class="d-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>toggle-switch</title><path d="M17,7H7A5,5 0 0,0 2,12A5,5 0 0,0 7,17H17A5,5 0 0,0 22,12A5,5 0 0,0 17,7M17,15A3,3 0 0,1 14,12A3,3 0 0,1 17,9A3,3 0 0,1 20,12A3,3 0 0,1 17,15Z" /></svg>
 <svg id="copy-toggle-off" style="width: 180px; height: 100px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>toggle-switch-off-outline</title><path d="M17 6H7c-3.31 0-6 2.69-6 6s2.69 6 6 6h10c3.31 0 6-2.69 6-6s-2.69-6-6-6zm0 10H7c-2.21 0-4-1.79-4-4s1.79-4 4-4h10c2.21 0 4 1.79 4 4s-1.79 4-4 4zM7 9c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z" /></svg>
 </div>
--- site/js/main.js
+++ site/js/main.js
@@ -79,13 +79,6 @@ function enlargeImg(id) {
         element.style.display = "none"
       }
 
-      // slides[slideIndex-1].style.transform = "scale(3)";
-      // slides[slideIndex-1].style.transition = "transform 0.25s ease";
-      // slides[slideIndex-1].style.marginTop = "5rem";
-      // imgIsBig = true;
-      // next.style.display = "none";
-      // prev.style.display = "none";
-      // caption.style.display = "none";
 
   }
   
@@ -99,7 +92,7 @@ function resetImg(id) {
 
   // MARK TEXT
   document.addEventListener('click', function() {
-    console.log(new CopyPermission());
+    //console.log(new CopyPermission());
     if (CopyPermission.getPermission()) {
       let popup = document.getElementById('copy-popup');
     // Prüfen, ob Text ausgewählt ist
@@ -114,11 +107,14 @@ function resetImg(id) {
           popup.classList.add("d-inline-flex");
           //popup.textContent = "Text copied:" + text;
          
-          popup.innerHTML = '<b>Copied text:</b><br>';
+          popup.innerHTML = '<b style="color: #7E56C2; font-size: 22px;">Copied text:</b><br>';
           //popup.innerHTML += text.replace('. ', '.<br>'); ///\n/g
           popup.innerHTML += text.replace(/\. |\;|\n/g, function(match) {
             return match === '\n' ? '<br>' : '.<br>';
         });
+          // popup.style.left = event.screenX + 'px';
+          // popup.style.top = event.screenY + 'px';
+          // ! Deprecated
           popup.style.left = event.clientX + 'px';
           popup.style.top = event.clientY + 'px';
           popup.style.display = 'inline-flex';
@@ -135,13 +131,24 @@ function resetImg(id) {
     }
   });
 
+  // document.addEventListener('mousemove', () => {
+  //   let popup = document.getElementById('copy-popup');
+  //     popup.style.left = (event.screenX + 5) + 'px';
+  //     popup.style.top = (event.screenY + 5) + 'px';
+  //     console.log(event.screenX + 'px');
+  // });
+
 
 
 setInterval(() => {
-  let popup = document.getElementById('copy-popup');
+  try {
+    let popup = document.getElementById('copy-popup');
   
-  popup.classList.remove("d-inline-flex");
-  popup.classList.add("d-none");
+    popup.classList.remove("d-inline-flex");
+    popup.classList.add("d-none");
+  } catch (e) {
+    console.log(e);
+  }
   
 }, 5000);
 
@@ -152,7 +159,7 @@ class CopyPermission {
   constructor() {
     console.log("CopyPermission constructor: " + this.permission );
     if (sessionStorage.getItem("copyPermission") === null) {
-      sessionStorage.setItem("copyPermission", confirm("On this page marking text automatically copies it to your clipboard.\nDo you agree?"));    
+      sessionStorage.setItem("copyPermission", confirm("On this page marking text automatically copies it to your clipboard.\nYou can change this everytime in the settings.\nDo you agree?"));    
     } 
     return sessionStorage.getItem("copyPermission") === "true";
     // if (this.permission === undefined || this.permission === null) {
@@ -163,7 +170,7 @@ class CopyPermission {
 
   static getPermission(){
     if (sessionStorage.getItem("copyPermission") === null) {
-      sessionStorage.setItem("copyPermission", confirm("On this page marking text automatically copies it to your clipboard.\nDo you agree?"));    
+      sessionStorage.setItem("copyPermission", confirm("On this page marking text automatically copies it to your clipboard.\nYou can change this everytime in the settings.\nDo you agree?"));    
     } 
     return sessionStorage.getItem("copyPermission") === "true";
     // if (this.permission === undefined || this.permission === null) {
@@ -174,3 +181,47 @@ class CopyPermission {
   }
 
 }
+
+
+// TABLES
+// document.getElementsByTagName('thead').foreach(thead => {
+//   thead.addEventListener('click', (event) => {
+//     // Überprüfen, ob auf ein TH-Element geklickt wurde
+//     console.log(event.target.tagName);
+//     if (event.target.tagName === 'TH') {
+//       // Finden Sie das dazugehörige TBODY-Element und schalten Sie seine Sichtbarkeit um
+//       this.closest('table').querySelector('tbody').style.display = this.closest('table').querySelector('tbody').style.display === 'none' ? '' : 'none';
+//     }
+//   });
+// });
+// Holen Sie sich alle 'thead'-Elemente
+const theads = document.getElementsByTagName('thead');
+
+// Fügen Sie jedem 'thead' einen Event-Listener hinzu
+Array.from(theads).forEach(thead => {
+  thead.addEventListener('click', function(event) {
+    // Überprüfen, ob auf ein TH-Element geklickt wurde
+    if (event.target.tagName === 'TH') {
+      // Finden Sie das dazugehörige TBODY-Element und schalten Sie seine Sichtbarkeit um
+      const tbody = this.closest('table').querySelector('tbody');
+      tbody.style.display = tbody.style.display === 'none' ? '' : 'none';
+    }
+  });
+});
+
+
+// function toggleTable(number){
+//   console.log(element);
+//   //element.parentElement.classList.add("d-none");
+
+
+
+//   document.getElementsByClassName('md-typeset__table').querySelector('thead').addEventListener('click', function(event) {
+//     // Überprüfen, ob auf ein TH-Element geklickt wurde
+//   console.log(event.target.tagName);
+//     if (event.target.tagName === 'TH') {
+//       // Finden Sie das dazugehörige TBODY-Element und schalten Sie seine Sichtbarkeit um
+//       this.closest('table').querySelector('tbody').style.display = this.closest('table').querySelector('tbody').style.display === 'none' ? '' : 'none';
+//     }
+//   });
+//}
